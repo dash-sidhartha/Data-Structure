167bcdd2fc2a4be852dbe5f7e4c88a51913e579d
Sidhartha Dash <sdash@teamdrg.com>
2016-11-29 19:50:07 +0530

    Details of Author Updated

---
+++ LinkedList/intro_LinkedList.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  -- This makes it possible to run the file as a script invoking the interpreter implicitly
+
+""" Introduction to Linked List """
+
+
+__author__ = "Sidhartha A. Dash"
+__version__ = "1.0"
+__email__ = "dash.sidhartha18@gmail.com"
+__contact__ = "91- 943 77 822 01"
+
+
