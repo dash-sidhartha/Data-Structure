edbc2eabe86abf01fdfaaed83b4e19b87cc21aa7
Sidhartha Dash <sdash@teamdrg.com>
2016-11-29 20:12:11 +0530

    Implementation Details

--- LinkedList/intro_LinkedList.py
+++ LinkedList/intro_LinkedList.py
@@ -8,4 +8,24 @@ __version__ = "1.0"
 __email__ = "dash.sidhartha18@gmail.com"
 __contact__ = "91- 943 77 822 01"
 
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
 
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def traverseList(self):
+		temp = self.head
+		while temp:
+			print(temp.data)
+			temp = temp.next
+
+if __name__ == '__main__':
+	ll = LinkedList()
+	ll.head = Node(3)
+	ll.head.next = Node(2)
+	ll.head.next.next = Node(1)
+	ll.traverseList()
